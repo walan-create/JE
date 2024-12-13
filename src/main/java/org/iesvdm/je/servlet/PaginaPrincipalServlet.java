bc75d532769ae68b14916319ead85cd73e7c28f3
walan-create <116348714+walan-create@users.noreply.github.com>
2024-12-13 01:38:19 +0100

    DAOS

---
+++ src/main/java/org/iesvdm/je/servlet/PaginaPrincipalServlet.java
@@ -0,0 +1,24 @@
+package org.iesvdm.je.servlet;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "PrincipalServlet", value = "/PrincipalServlet")
+public class PaginaPrincipalServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/listadoSociosB.jsp");
+        dispatcher.forward(request, response);
+
+    }
+
+}
